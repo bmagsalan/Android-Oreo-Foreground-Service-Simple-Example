a27b0a572acfee31a511ed04511f5ea86173ce6e
DESKTOP-1Q28S2O\Brian <bmagsalan@yahoo.ca>
2020-11-06 12:43:29 -0500

    Removed location service.

--- app/src/main/java/com/data/studysensor/androidoreoforegroundtest/MainActivity.java
+++ app/src/main/java/com/data/studysensor/androidoreoforegroundtest/MainActivity.java
@@ -2,25 +2,12 @@ package com.data.studysensor.androidoreoforegroundtest;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.StrictMode;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import com.data.studysensor.locationupdate.LocationUpdatesService;
-import com.data.studysensor.timetracker.MyApplication;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends Activity {
 
