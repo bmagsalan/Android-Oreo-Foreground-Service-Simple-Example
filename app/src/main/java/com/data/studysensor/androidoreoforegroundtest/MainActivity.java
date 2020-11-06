35c983d407fca3f97a2dc280c8aa61cef61eb443
DESKTOP-1Q28S2O\Brian <bmagsalan@yahoo.ca>
2020-11-06 13:17:55 -0500

    Consolidated MainActivity.

--- app/src/main/java/com/data/studysensor/androidoreoforegroundtest/MainActivity.java
+++ app/src/main/java/com/data/studysensor/androidoreoforegroundtest/MainActivity.java
@@ -4,10 +4,22 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
+
+import com.data.studysensor.timetracker.MyApplication;
+import com.data.studysensor.timetracker.TimeSheetDayActivity;
+import com.data.studysensor.timetracker.TimeSheetMonthActivity;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends Activity {
 
@@ -40,16 +52,43 @@ public class MainActivity extends Activity {
                     startService(startIntent);
                 }
 
-                Intent intent = new Intent(MainActivity.this, TimeTrackerActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-
-                finish();
 
             }
         });
 
 
     }
+
+    public void sendEmail(View view) {
+                ((MyApplication)getApplication()).save();
+         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+        File filelocation = new File(Environment.getExternalStoragePublicDirectory(""), "timesheet.csv");
+        Uri path  = Uri.fromFile(filelocation);
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("vnd.android.cursor.dir/email");
+        String[] to = new String[]{"brian@issist.com"};
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, to);
+        emailIntent.putExtra(Intent.EXTRA_STREAM, path);
+
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Time Sheet");
+        startActivity(Intent.createChooser(emailIntent, "Send email..."));
+    }
+
+
+    public void startHoursActivity(View view) {
+        Intent intent = new Intent(this, TimeSheetDayActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void startMonthlyHoursActivity(View view) {
+        Intent intent = new Intent(this, TimeSheetMonthActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
+
 
 }
